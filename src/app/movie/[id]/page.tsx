92e9a323193b7011eeef7c1cbf2f89841c898733
thuanbua562004 <vanthuan562004@gmail.com>
2025-06-09 19:57:12 +0700

    d

--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -2,29 +2,23 @@ import { Metadata } from 'next';
 import MovieDetail from '@/app/component/MovieDetail';
 import { redirect } from 'next/navigation';
 
-type PageProps = {
-  params: { id: string };
-  searchParams?: { [key: string]: string | string[] | undefined };
-};
+export async function generateMetadata({ params }: { params: { id: number } }): Promise<Metadata> {
+  const { id } = await params
+  const response = await fetch(`https://ophim1.com/phim/${id}`);
+  const data = await response.json();
+  
+  return {
+    title:  'Phim hay nhất : ' + data.movie.name  || 'Phim hay nhất' ,
+    description: data.movie.origin_name || 'Xem phim miễn phí chất lượng cao!',
+  };
+}
 
-// export async function generateMetadata({ params }: PageProps): Promise<Metadata> {
-//   const { id } = params;
-//   const response = await fetch(`https://ophim1.com/phim/${id}`);
-//   const data = await response.json();
-
-//   return {
-//     title: 'Phim hay nhất: ' + (data.movie?.name || 'Phim hay nhất'),
-//     description: data.movie?.origin_name || 'Xem phim miễn phí chất lượng cao!',
-//   };
-// }
-
-export default async function Page({ params }: PageProps) {
-  const { id } = params;
+export default async function Page({ params }: { params: { id: number } }) {
+  const { id } = await params
   const response = await fetch(`https://ophim1.com/phim/${id}`);
   const movieDetail = await response.json();
-
-  if (movieDetail.status === false) {
-    redirect('/');
+  if(movieDetail.status == false){
+    redirect('/')
   }
 
   return (
